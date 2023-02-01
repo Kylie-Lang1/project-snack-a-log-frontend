ec99543b67666b912e3d21967d35000ac6d0db53
Kylie-Lang1 <kylielang@pursuit.org>
2023-02-01 09:25:49 -0500

    created tailwind style sheet

---
+++ src/css/styles.js
@@ -0,0 +1,19 @@
+// File for tailwindcss style consts
+
+// css for reused components
+export const button = "py-2 px-4 mx-2 hover:shadow-lg shadow-indigo-500/50"
+export const heart = "h-14 pl-10 mt-16"
+
+// css for nav bar
+export const nav = "flex flex-row w-full bg-indigo-900 justify-items-stretch"
+export const nav_home = "w-full pl-10 text-orange-500 h-32 text-7xl items-center flex flex-auto"
+export const nav_button = "w-96 pr-10 text-white h-32 text-5xl items-center justify-end flex flex-auto"
+
+// css for show page with snack details
+export const show_h2 = "text-7xl my-16 mx-16 text-center tracking-wider"
+
+export const details_page = "flex justify-center"
+export const details_img = "float-left ml-40 mr-20 border-double border-8 border-stone-500"
+export const details_head = "border-b-2 border-black pb-6"
+export const details_h1 = "text-6xl float-left mb-10"
+export const info = "text-3xl pb-5"
