802e772f21218237d83f9e9f34ca2136cfa24903
TonyH98 <tonyhoang@pursuit.org>
2023-02-02 11:10:53 -0500

    Multiple Delete Work

--- src/Components/Snacks.js
+++ src/Components/Snacks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Snack from "./Snack";
 
@@ -12,6 +12,9 @@ export default function Snacks() {
   const [allSnacks, setAllSnacks] = useState([]);
   const [selectedSnacks, setSelectedSnacks] = useState([]);
   const [searchSnack, setSearchSnack] = useState("");
+
+
+let navigate = useNavigate()
 
   // useEffect creates an inital state of all snack objects within an array
   useEffect(() => {
@@ -87,17 +90,19 @@ for(let i = 0 ; i < arr.length; i++){
       const copySnackArray = [...snacks];
       const indexDeletedSnacks = copySnackArray.map((snack , i) => {
         return(
-          snack.arr === arr ? i : []
+          snack.id === arr[i] ? i : []
         ) 
       });
       for(let j = 0; j < arr.length ; j++){
         copySnackArray.splice(indexDeletedSnacks, 1);
         setSnacks(copySnackArray);
-
       }
     },
     (error) => console.error(error)
   )
+  .then(() => {
+    navigate(`/snacks`)
+  })
   .catch((c) => console.warn("catch", c));
 }
 }
