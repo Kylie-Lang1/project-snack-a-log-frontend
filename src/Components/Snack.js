1d3b9cf6d8f7d76aaf1baa0f25e76df5b4ad46a4
Kylie-Lang1 <kylielang@pursuit.org>
2023-02-02 21:11:14 -0500

    formatting index cards

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -2,10 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import regular from "../assets/heart-regular.png";
 import solid from "../assets/heart-solid.png";
+import { FaBookmark } from "react-icons/fa";
+import { FaRegBookmark } from "react-icons/fa";
+import * as tailwind from "../css/styles"
 
-export default function Snack({ snack, isFavorite }) {
+export default function Snack({ snack }) {
+
+  const handleSelect = (e) => {
+      let value = e.target.value
+      snack.selected = value
+      return snack
+  }
+
   return (
-    <section className="m-4 bg-white shadow-2xl rounded-lg">
+  <>
+    <section className="m-4 bg-white shadow-2xl rounded-lg w-96 h-96">
       <div>
         <Link to={`/snacks/${snack.id}`}>
           <img
@@ -14,29 +25,36 @@ export default function Snack({ snack, isFavorite }) {
             className="object-cover h-80 w-full rounded-t-lg"
           ></img>
         </Link>
-        <h5 className="inline w-96 break-all ml-1">
-          <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
-        </h5>
-        {snack.is_healthy ? (
-          <span>
-            <img src={solid} alt="solid" className="w-10 inline ml-4"></img>
-          </span>
-        ) : (
-          <span>
-            <img src={regular} alt="regular" className="w-10 inline ml-4"></img>
-          </span>
-        )}
-        <div>
-          <input
-          type="checkbox"
-          onChange={(e) => {
-            let value = e.target.value
-            snack.selected = value
-            return snack
-          }}
-          />
+        <div className="flex items-center relative">
+          <div className="flex flex-auto items-center">
+            <input
+              type="checkbox"
+              onChange={handleSelect}
+              className="float-left item-align-center ml-2"
+              />
+            <h5 className="inline break-normal px-2 py-2 truncate w-64"> 
+              <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
+            </h5>
+          </div>
+          <div className="flex flex-auto items-center justify-end pr-3">
+            {snack.is_healthy ? (
+              <span>
+                <img src={solid} alt="solid" className="h-8 w-10 pr-3"></img>
+              </span>
+            ) : (
+              <span>
+                <img src={regular} alt="regular" className="h-8 w-10 pr-3"></img>
+              </span>
+            )}
+            {
+              snack.bookmarked ?
+              <FaBookmark className={tailwind.index_bookmark}/> :
+              <FaRegBookmark className={tailwind.index_bookmark} />
+            }
+          </div>
         </div>
       </div>
     </section>
+  </>
   );
 }
--- src/css/styles.js
+++ src/css/styles.js
@@ -2,7 +2,6 @@
 
 // css for reused components
 export const button = "py-2 px-4 mx-2 hover:shadow-lg shadow-indigo-500/50"
-export const heart = "h-14 pl-10 mt-16"
 
 // css for nav bar
 export const nav = "flex flex-row w-full bg-indigo-900 justify-items-stretch"
@@ -17,6 +16,7 @@ export const details_img = "mr-20 mt-10 border-double border-8 border-stone-500 
 export const details_head = "border-b-2 border-black pb-6"
 export const details_h3 = "text-6xl float-left mb-10"
 export const info = "text-3xl pb-5"
+export const heart = "h-14 pl-10 mt-16"
 
 export const bookmark = "absolute bottom--10 left-5 scale-150 mt-5"
 export const bookmark_text = "text-blue-500 absolute bottom--10 left-10 ml-5 border-none hover:underline hover:text-blue-500"
@@ -24,4 +24,5 @@ export const bookmark_text = "text-blue-500 absolute bottom--10 left-10 ml-5 bor
 // css for index page
 export const index_h3 = "text-left text-gray-700 font-bold text-4xl w-full my-10 ml-16 justify-left"
 export const index_heart = "h-14 pl-10 mt-16"
+export const index_bookmark = "float-right scale-125 right-3"
 
