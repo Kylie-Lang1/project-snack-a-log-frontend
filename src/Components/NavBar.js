b770521a378ad5a91bec8f9a03721e50805b03cb
Fadila-Ali <fadilaali@pursuit.org>
2023-02-02 20:49:21 -0500

    logo done

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import * as tailwind from "../css/styles";
+import Logo from "../assets/icons8-nachos-50.png";
 
 function NavBar() {
-    return (
-        <nav className={tailwind.nav}>
-            <Link to="/" className={tailwind.nav_home}><h1>Snack-A-Log</h1></Link>
-            <Link to="/snacks/new" className={tailwind.nav_button}><h2>Add Snack</h2></Link>
-            <Link to="/snacks" className={tailwind.nav_button}><h2>All Snacks</h2></Link>
-        </nav>
-    );
+  return (
+    <nav className={tailwind.nav}>
+      <Link to="/" className={tailwind.nav_home}>
+        <span className="before:block before:absolute before:-inset-1 before:-skew-y-6 before:bg-orange-500 relative inline-block rounded-lg mr-2 z-0">
+          <img src={Logo} className="z-10 relative text-white rounded-lg"></img>
+        </span>
+        <h1>
+          <i>i</i>Snack
+        </h1>
+      </Link>
+      <Link to="/snacks/new" className={tailwind.nav_button}>
+        <h2>Add Snack</h2>
+      </Link>
+      <Link to="/snacks" className={tailwind.nav_button}>
+        <h2>All Snacks</h2>
+      </Link>
+    </nav>
+  );
 }
 
 export default NavBar;
